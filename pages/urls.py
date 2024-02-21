676b73df02ca96d4c4484d365d8769639290050e
Aidan Keefe <aidanbkeefe@gmail.com>
2024-02-21 15:08:48 -0500

    updated page views and connections

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-    path("", views.home, name='home')
+    path("", views.home, name='home'),
+    path("game/", views.game, name='game')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, "pages/home.html", {})
 
+def game(request):
+    return render(request, "pages/game.html", {})
