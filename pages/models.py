b933cad810d4a22d28d7f4c63f9af4e679bfa546
Aidan Keefe <aidanbkeefe@gmail.com>
2024-04-19 15:56:37 -0400

    updating database functionality

--- pages/models.py
+++ pages/models.py
@@ -2,8 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Portfolio(models.Model):
-    stock_name = models.CharField(max_length=10)
-    num_shares = models.IntegerField()
+    aal = models.IntegerField()
+    aapl = models.IntegerField()
+    amzn = models.IntegerField()
+    bac = models.IntegerField()
+    dal = models.IntegerField()
+    hmc = models.IntegerField()
+    jnj = models.IntegerField()
+    jpm = models.IntegerField()
+    lly = models.IntegerField()
+    luv = models.IntegerField()
+    msft = models.IntegerField()
+    tm = models.IntegerField()
+    tsla = models.IntegerField()
+    unh = models.IntegerField()
+    v = models.IntegerField()
 
     def __str__(self):
-        return f"{self.stock_name - self.num_shares}"
+        return f"{self.aal - self.aapl - self.amzn - self.bac - self.dal - self.hmc - self.jnj - self.jpm - self.lly - self.luv - self.msft - self.tm - self.tsla - self.unh - self.v}"
